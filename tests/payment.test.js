6710c4e88a3aac61a584227b1b09f97dd09bb784
jonas-silva-qm <jonas.davila@qualitymap.io>
2025-04-07 10:10:26 -0300

    feat: adiciona payment com teste assÃ­ncrono

--- tests/payment.test.js
+++ tests/payment.test.js
@@ -4,8 +4,6 @@ test('payment loads', () => {
   expect(payment()).toBe('payment feature loaded');
 });
 
-// Teste assÃ­ncrono simulado
-
 test('payment async resolves', async () => {
   await new Promise(r => setTimeout(r, 100));
   expect(true).toBe(true);
