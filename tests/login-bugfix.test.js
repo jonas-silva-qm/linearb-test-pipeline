7a8c9523d01309d91a2641adbdcc244b00f2a393
jonas-silva-qm <jonas.davila@qualitymap.io>
2025-04-10 10:03:35 -0300

    fix: validaÃ§Ã£o login com teste assÃ­ncrono simulado

---
+++ tests/login-bugfix.test.js
@@ -0,0 +1,6 @@
+﻿import { login } from '../src/login.js';
+
+test('login falha com delay', async () => {
+  await new Promise(r => setTimeout(r, 200));
+  expect(login('user', 'wrong')).toBe(false);
+});
